b6ab61444b0a37a243d75de6147a961c36fad77f
Marcelo Ortiz Peredo <a2018119133@estudiantes.upsa.edu.bo>
2021-02-27 12:02:02 -0400

    Generador de intentidades con el modelo Identity y el controlador IdentitiesController echo(Programa de lista con Api .Net Core)

---
+++ GeneradorIdentidades/GeneradorIdentidades/Models/Identity.cs
@@ -0,0 +1,11 @@
+﻿namespace GeneradorIdentidades.Models
+{
+    public class Identity
+    {
+        public int IdentityId { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public string Sex { get; set; }
+
+    }
+}
